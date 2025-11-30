b5ea8c831354e51d5a8d46d86b803407f3ca3f7f
eemiljka <eemiljka@metropolia.fi>
2025-11-30 20:44:56 +0200

    simple database connection done

---
+++ db.ts
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'myprogress'
+});
+
+console.log("HOST: ", process.env.MYSQL_HOST);
+console.log("USER: ", process.env.MYSQL_USER);
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Error connecting to MySQL:", err);
+        return;
+    }
+    console.log("Connected to MySQL database 'myprogress'");
+});
+
+module.exports = connection;
--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,9 @@ app.get("/health", (req: Request, res: Response) => {
   res.json({ status: "OK" });
 });
 
+app.get("/mysets", (req: Request, res: Response) => {
+  res.send({ message: "Here are your sets" })
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
